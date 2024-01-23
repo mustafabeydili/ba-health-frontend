bb57e33c564f2c6248a0d5f2c2a34c3372a7f8ec
mustafabeydili <beydilimustafa@gmail.com>
2024-01-23 21:34:06 +0300

    banner search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Input } from 'antd';
 
-
+const { Search } = Input;
 export default class Home extends React.Component {
   state = {
   };
@@ -8,7 +9,30 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        Home
+        <div className="row">
+        <div className="slogan col-lg-5 col-md-4 col-sm-12 col-xs-12">
+          <h1>
+          COMPARE CLINIC, HOTEL, TRANSFER
+          CAR RENTAL COMPANIES,
+          AND BOOK ONLINE
+          </h1>
+          <h5>
+          Healthgate: Akredite klinikleri , konforlu ve en uygun otelleri , istediğiniz
+          tarzda aracı ve keyfinize göre bir tatil programını bulun , karşılaştırın ve
+          çevrimiçi rezervasyon yapın. Türkiye'de sağlık hizmeti ile birlikte bütüncül
+          hizmet almanın en güvenli yolu. Dünya çapında 100.000'den fazla hasta
+          tarafından güvenilmektedir.
+          </h5>
+        </div>
+         
+          <div className=" col-lg-7 col-md-8 col-sm-12 col-xs-12">
+          
+        </div>
+        </div> 
+        <div className="search">
+            <Search placeholder="Hastane , Hotel , Araç Arayın" enterButton="Search" size="large" loading />
+        </div>
+
         </div>
     );
   }
